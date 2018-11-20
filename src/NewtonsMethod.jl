61c814b9834ab5a889f140e501846cf679300493
Max Norton <maxbnorton@gmail.com>
2018-11-19 17:36:18 -0800

    add 2 newtonroot methods

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,22 @@
 module NewtonsMethod
 
-greet() = print("Hello World!")
+function newtonroot(f, f′, x₀; tol=1E-7, maxiter=1000)
+    x_old = x₀
+    normdiff = Inf
+    iter = 1
+    while normdiff > tol && iter <= maxiter
+        x_new = x_old - f(x_old)/f′(x_old)
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter += 1
+    end
+    return (x_old, normdiff, iter)
+end
+
+function newtonroot(f, x₀; tol=1E-7, maxiter=1000)
+    Diff(f) = x -> ForwardDiff.derivative(f, x)
+    f′ = Diff(f)
+    newtonroot(f, f′, x₀)
+end
 
 end # module
