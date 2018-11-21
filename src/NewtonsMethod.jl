e9aad61432021a074ea33f87a167427ccadd431a
Max Norton <maxbnorton@gmail.com>
2018-11-21 13:27:10 -0800

    add ForwardDiff

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,6 +1,6 @@
 module NewtonsMethod
 
-using LinearAlgebra, Statistics, Compat
+using LinearAlgebra, Statistics, Compat, ForwardDiff
 
 function newtonroot(f, f′, x₀; tol=1E-7, maxiter=1000)
     x_old = x₀
@@ -8,7 +8,7 @@ function newtonroot(f, f′, x₀; tol=1E-7, maxiter=1000)
     iter = 1
     while normdiff > tol && iter <= maxiter
         x_new = x_old - f(x_old)/f′(x_old)
-        normdiff = norm(x_new - x_old)
+        normdiff = x_new - x_old
         x_old = x_new
         iter += 1
     end
