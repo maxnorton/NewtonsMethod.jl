cbed0238734c6ce1bb13f2426e9720b67f297b12
Max Norton <maxbnorton@gmail.com>
2018-11-21 23:28:20 -0800

    Revert "break iter test for tol=Inf"
    This reverts commit ab16b7ea2d51075edacf4646a0befd4b46f403e0.

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -6,7 +6,7 @@ function newtonroot(f, f′, x₀; tol=1E-7, maxiter=1000)
     x_old = x₀
     normdiff = Inf
     iter = 1
-    while normdiff >= tol && iter <= maxiter
+    while normdiff > tol && iter <= maxiter
         x_new = x_old - f(x_old)/f′(x_old)
         normdiff = norm(x_new - x_old)
         x_old = x_new
